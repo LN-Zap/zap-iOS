e31d29b466bfba9df7540940580e0a00539dc133
Otto Suess <ottosuess@protonmail.com>
2018-05-09 15:38:09 +0200

    customize debug messages

--- Zap/Lnd/LndConfiguration.swift
+++ Zap/Lnd/LndConfiguration.swift
@@ -12,7 +12,7 @@ final class LndConfiguration: NSObject {
     
     static let standard: LndConfiguration = {
         let configuration = LndConfiguration()
-        configuration.set("Application Options", key: "debuglevel", value: "info")
+        configuration.set("Application Options", key: "debuglevel", value: "ATPL=debug,BRAR=debug,BTCN=info,CHDB=debug,CMGR=debug,CNCT=debug,CRTR=warn,DISC=debug,FNDG=debug,HSWC=debug,LNWL=debug,LTND=debug,NTFN=debug,PEER=info,RPCS=debug,SPHX=debug,SRVR=debug,UTXN=debug")
         configuration.set("Application Options", key: "maxpendingchannels", value: "10")
         configuration.set("Application Options", key: "nobootstrap", value: "1")
         configuration.set("Application Options", key: "noencryptwallet", value: "1")
