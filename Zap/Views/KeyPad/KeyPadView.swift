e34326a07860effedfa440468e868dae1180bacb
Otto Suess <ottosuess@protonmail.com>
2018-05-12 16:17:38 +0200

    remove keypad logging

--- Zap/Views/KeyPad/KeyPadView.swift
+++ Zap/Views/KeyPad/KeyPadView.swift
@@ -67,7 +67,6 @@ final class KeyPadView: UIView {
     var numberString = "" {
         didSet {
             guard oldValue != numberString else { return }
-            print(numberString)
         }
     }
     
