823aac8c33fbef141f115260bf7a94e5a980e7e3
Otto Suess <ottosuess@protonmail.com>
2018-05-14 14:18:13 +0200

    add exchange rate to settings screen

--- Zap/Scenes/Settings/SettingsContainerViewController.swift
+++ Zap/Scenes/Settings/SettingsContainerViewController.swift
@@ -5,12 +5,14 @@
 //  Copyright © 2018 Zap. All rights reserved.
 //
 
+import BTCUtil
 import UIKit
 
 class SettingsContainerViewController: UIViewController, ContainerViewController {
     @IBOutlet private weak var headerBackground: UIView!
     @IBOutlet private weak var primaryCurrencyLabel: UILabel!
     @IBOutlet private weak var secondaryCurrencyLabel: UILabel!
+    @IBOutlet private weak var exchangeRateLabel: UILabel!
     
     // swiftlint:disable:next private_outlet
     @IBOutlet weak var container: UIView?
@@ -36,10 +38,11 @@ class SettingsContainerViewController: UIViewController, ContainerViewController
             .bind(to: secondaryCurrencyLabel.reactive.text, currency: Settings.secondaryCurrency)
             .dispose(in: reactive.bag)
         
-        Style.label.apply(to: primaryCurrencyLabel, secondaryCurrencyLabel)
+        Style.label.apply(to: primaryCurrencyLabel, secondaryCurrencyLabel, exchangeRateLabel)
         primaryCurrencyLabel.textColor = .white
         primaryCurrencyLabel.font = primaryCurrencyLabel.font.withSize(30)
         secondaryCurrencyLabel.textColor = .gray
+        exchangeRateLabel.textColor = .gray
 
         let viewController = SettingsViewController()
         setInitialViewController(viewController)
@@ -47,7 +50,22 @@ class SettingsContainerViewController: UIViewController, ContainerViewController
         title = "scene.settings.title".localized
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateExchangeRateLabel()
+    }
+    
     @IBAction private func closeButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    private func updateExchangeRateLabel() {
+        let bitcoin = Bitcoin(unit: .bitcoin)
+        let satoshis: Satoshi = 100_000_000
+        
+        if let bitcoin = bitcoin.format(satoshis: satoshis),
+            let fiat = Settings.fiatCurrency.value.format(satoshis: satoshis) {
+            exchangeRateLabel.text = "\(bitcoin) = \(fiat)"
+        }
     }
 }
