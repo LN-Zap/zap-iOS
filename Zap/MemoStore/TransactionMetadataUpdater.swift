8824f1de1b5f6977ff73244c12a8ee6c12b27115
Otto Suess <ottosuess@protonmail.com>
2018-05-16 20:23:04 +0200

    remove broken code

--- Zap/MemoStore/TransactionMetadataUpdater.swift
+++ Zap/MemoStore/TransactionMetadataUpdater.swift
@@ -9,32 +9,32 @@ import Bond
 import Foundation
 import ReactiveKit
 
-final class TransactionMetadataUpdater: NSObject {
-    let viewModel: ViewModel
-    
-    init(viewModel: ViewModel, transactionMetadataStore: TransactionMetadataStore) {
-        self.viewModel = viewModel
-        super.init()
-        
-        combineLatest(viewModel.channels.all, viewModel.transactions)
-            .observeNext { channels, transactions in
-                for transaction in transactions where transactionMetadataStore.metadata(for: transaction) == nil {
-                    // search for matching channel for funding transaction
-                    guard let channel = channels.first(where: { $0.channelPoint.hasPrefix(transaction.id) }) else { continue }
-                    
-                    viewModel.nodeInfo(pubKey: channel.remotePubKey) { result in
-                        guard var alias = result.value?.node.alias else { return }
-                        
-                        // sometimes lnd returns an empty alias as nodeInfo response
-                        if alias == "" {
-                            alias = channel.remotePubKey
-                        }
-                        
-                        let metadata = TransactionMetadata(memo: nil, fundingChannelAlias: alias)
-                        transactionMetadataStore.setMetadata(metadata, for: transaction)
-                    }
-                }
-            }
-            .dispose(in: reactive.bag)
-    }
-}
+//final class TransactionMetadataUpdater: NSObject {
+//    let viewModel: ViewModel
+//    
+//    init(viewModel: ViewModel, transactionMetadataStore: TransactionMetadataStore) {
+//        self.viewModel = viewModel
+//        super.init()
+//        
+//        combineLatest(viewModel.channels.all, viewModel.transactions)
+//            .observeNext { channels, transactions in
+//                for transaction in transactions where transactionMetadataStore.metadata(for: transaction) == nil {
+//                    // search for matching channel for funding transaction
+//                    guard let channel = channels.first(where: { $0.channelPoint.hasPrefix(transaction.id) }) else { continue }
+//                    
+//                    viewModel.nodeInfo(pubKey: channel.remotePubKey) { result in
+//                        guard var alias = result.value?.node.alias else { return }
+//                        
+//                        // sometimes lnd returns an empty alias as nodeInfo response
+//                        if alias == "" {
+//                            alias = channel.remotePubKey
+//                        }
+//                        
+//                        let metadata = TransactionMetadata(memo: nil, fundingChannelAlias: alias)
+//                        transactionMetadataStore.setMetadata(metadata, for: transaction)
+//                    }
+//                }
+//            }
+//            .dispose(in: reactive.bag)
+//    }
+//}
