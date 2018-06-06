9fc5f727afa4e873305579cdd88ee9219054c23c
Otto Suess <ottosuess@protonmail.com>
2018-06-06 10:44:23 +0200

    [detail] remove delete button from detail vc

--- Zap/Scenes/Detail/DetailViewController.swift
+++ Zap/Scenes/Detail/DetailViewController.swift
@@ -9,7 +9,6 @@ import Bond
 import UIKit
 
 class DetailViewController: UIViewController {
-    @IBOutlet private weak var hideTransactionButton: UIButton!
     @IBOutlet private weak var tableView: UITableView!
     
     var detailViewModel: DetailViewModel?
@@ -24,11 +23,6 @@ class DetailViewController: UIViewController {
         
         title = detailViewModel?.detailViewControllerTitle
         titleTextStyle = .dark
-        
-        Style.button.apply(to: hideTransactionButton)
-        
-        hideTransactionButton.setTitle("delete", for: .normal)
-        hideTransactionButton.tintColor = UIColor.zap.tomato
         
         tableView.registerCell(DetailTableViewCell.self)
         tableView.registerCell(DetailMemoTableViewCell.self)
@@ -45,51 +39,47 @@ class DetailViewController: UIViewController {
         tableView.separatorStyle = .none
         
         detailViewModel?.detailCells
-            .bind(to: tableView) { data, indexPath, tableView -> UITableViewCell in
-                switch data[indexPath.row] {
-                case .info(let info):
-                    let cell: DetailTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
-                    cell.info = info
-                    return cell
-                case .memo(let info):
-                    let cell: DetailMemoTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
-                    cell.onChange = { [weak self] in
-                        guard let transactionViewModel = self?.detailViewModel as? TransactionViewModel else { return }
-                        self?.viewModel?.udpateMemo($0, for: transactionViewModel)
-                    }
-                    cell.info = info
-                    return cell
-                case .qrCode(let address):
-                    let cell: DetailQRCodeTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
-                    cell.address = address
-                    cell.delegate = self
-                    return cell
-                case .timer(let info):
-                    let cell: DetailTimerTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
-                    cell.info = info
-                    return cell
-                case .legend(let info):
-                    let cell: DetailLegendTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
-                    cell.info = info
-                    return cell
-                case .balance(let info):
-                    let cell: DetailBalanceTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
-                    cell.info = info
-                    return cell
-                case .separator:
-                    return tableView.dequeueCellForIndexPath(indexPath) as DetailSeparatorTableViewCell
-                }
+            .bind(to: tableView, createCell: createCell)
+            .dispose(in: reactive.bag)
+    }
+    
+    private func createCell(data: ObservableArray<DetailCellType>, indexPath: IndexPath, tableView: UITableView) -> UITableViewCell {
+        switch data[indexPath.row] {
+        case .info(let info):
+            let cell: DetailTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
+            cell.info = info
+            return cell
+        case .memo(let info):
+            let cell: DetailMemoTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
+            cell.onChange = { [weak self] in
+                guard let transactionViewModel = self?.detailViewModel as? TransactionViewModel else { return }
+                self?.viewModel?.udpateMemo($0, for: transactionViewModel)
             }
+            cell.info = info
+            return cell
+        case .qrCode(let address):
+            let cell: DetailQRCodeTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
+            cell.address = address
+            cell.delegate = self
+            return cell
+        case .timer(let info):
+            let cell: DetailTimerTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
+            cell.info = info
+            return cell
+        case .legend(let info):
+            let cell: DetailLegendTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
+            cell.info = info
+            return cell
+        case .balance(let info):
+            let cell: DetailBalanceTableViewCell = tableView.dequeueCellForIndexPath(indexPath)
+            cell.info = info
+            return cell
+        case .separator:
+            return tableView.dequeueCellForIndexPath(indexPath) as DetailSeparatorTableViewCell
+        }
     }
     
     @IBAction private func doneButtonTapped(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // TODO: move to cell
-    @IBAction private func hideTransaction(_ sender: Any) {
-        guard let transactionViewModel = detailViewModel as? TransactionViewModel else { return }
-        viewModel?.hideTransaction(transactionViewModel)
         dismiss(animated: true, completion: nil)
     }
 }
