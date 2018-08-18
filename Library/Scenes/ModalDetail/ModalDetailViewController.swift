f7763ad4956e7ebb398c8e51fa436ef880cd0c98
Otto Suess <ottosuess@protonmail.com>
2018-08-18 11:16:08 +0200

    refactor: refactor modal detail vc

--- Library/Scenes/ModalDetail/ModalDetailViewController.swift
+++ Library/Scenes/ModalDetail/ModalDetailViewController.swift
@@ -8,74 +8,76 @@
 import UIKit
 
 class ModalDetailViewController: ModalViewController, QRCodeScannerChildViewController {
-    private weak var backgroundView: UIView?
-    private weak var contentStackView: UIStackView?
-    private weak var headerIconImageView: UIImageView?
+    private let closeButton: UIButton = {
+        let closeButton = UIButton(type: .custom)
+        closeButton.setImage(UIImage(named: "icon_close", in: Bundle.library, compatibleWith: nil), for: .normal)
+        closeButton.addTarget(self, action: #selector(dismissParent), for: .touchUpInside)
+        return closeButton
+    }()
+    
+    private let backgroundView: UIView = {
+        let backgroundView = UIView(frame: CGRect.zero)
+        backgroundView.layer.cornerRadius = 14
+        backgroundView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        backgroundView.backgroundColor = UIColor.Zap.seaBlue
+        return backgroundView
+    }()
+    
+    private let contentStackView: UIStackView = {
+        let contentStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        contentStackView.axis = .vertical
+        contentStackView.spacing = 14
+        return contentStackView
+    }()
+    
+    private let headerIconImageView: UIImageView = {
+        let headerIconImageView = UIImageView(image: nil)
+        headerIconImageView.alpha = 0
+        return headerIconImageView
+    }()
     
     weak var delegate: QRCodeScannerChildDelegate?
     
     var stackViewContent: [StackViewElement] = [] {
         didSet {
-            contentStackView?.set(elements: stackViewContent)
-            updateHeight()
+            contentStackView.set(elements: stackViewContent)
         }
     }
     
-    private func setupLayout() {
-        let backgroundView = UIView(frame: CGRect.zero)
-        backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(backgroundView)
+    private func setupLayout(for view: UIView) {
+        view.addAutolayoutSubview(backgroundView)
         backgroundView.constrainEdges(to: view)
-        backgroundView.layer.cornerRadius = 14
-        backgroundView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        backgroundView.backgroundColor = UIColor.Zap.seaBlue
-        self.backgroundView = backgroundView
-        
-        let closeButton = UIButton(type: .custom)
-        closeButton.translatesAutoresizingMaskIntoConstraints = false
-        closeButton.setImage(UIImage(named: "icon_close", in: Bundle.library, compatibleWith: nil), for: .normal)
-        backgroundView.addSubview(closeButton)
+
+        backgroundView.addAutolayoutSubview(closeButton)
         NSLayoutConstraint.activate([
             closeButton.widthAnchor.constraint(equalToConstant: 45),
             closeButton.heightAnchor.constraint(equalToConstant: 45),
             closeButton.topAnchor.constraint(equalTo: backgroundView.topAnchor),
             closeButton.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor)
         ])
-        closeButton.addTarget(self, action: #selector(dismissParent), for: .touchUpInside)
         
-        let headerIconImageView = UIImageView(image: nil)
-        headerIconImageView.translatesAutoresizingMaskIntoConstraints = false
-        headerIconImageView.alpha = 0
-        view.addSubview(headerIconImageView)
+        view.addAutolayoutSubview(headerIconImageView)
         NSLayoutConstraint.activate([
             headerIconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             headerIconImageView.centerYAnchor.constraint(equalTo: view.topAnchor)
         ])
-        self.headerIconImageView = headerIconImageView
         
-        let contentStackView = UIStackView()
-        contentStackView.axis = .vertical
-        contentStackView.spacing = 14
-        contentStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 15, leading: 20, bottom: 0, trailing: 20)
-        contentStackView.isLayoutMarginsRelativeArrangement = true
-        contentStackView.translatesAutoresizingMaskIntoConstraints = false
-        backgroundView.addSubview(contentStackView)
+        backgroundView.addAutolayoutSubview(contentStackView)
         NSLayoutConstraint.activate([
-            contentStackView.topAnchor.constraint(equalTo: headerIconImageView.bottomAnchor),
-            contentStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor),
-            contentStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor)
+            contentStackView.topAnchor.constraint(equalTo: headerIconImageView.bottomAnchor, constant: 15),
+            contentStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 20),
+            contentStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20)
         ])
-        self.contentStackView = contentStackView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupLayout()
-        
+
+        setupLayout(for: view)
+
         view.clipsToBounds = false
         
-        headerIconImageView?.image = UIImage(named: "icon_header_lightning", in: Bundle.library, compatibleWith: nil)
+        headerIconImageView.image = UIImage(named: "icon_header_lightning", in: Bundle.library, compatibleWith: nil)
     }
     
     func updateHeight() {
@@ -98,19 +100,19 @@ class ModalDetailViewController: ModalViewController, QRCodeScannerChildViewCont
 
 extension ModalDetailViewController: ContentHeightProviding {
     var contentHeight: CGFloat? {
-        let headerImageMargin = (headerIconImageView?.image?.size.height ?? 0) / 2
+        let headerImageMargin = (headerIconImageView.image?.size.height ?? 0) / 2
         let topMargin: CGFloat = 15
         let bottomMargin: CGFloat = 34
-        return stackViewContent.height(spacing: contentStackView?.spacing ?? 0) + headerImageMargin + topMargin + bottomMargin
+        return stackViewContent.height(spacing: contentStackView.spacing) + headerImageMargin + topMargin + bottomMargin
     }
 }
 
 extension ModalDetailViewController: ModalTransitionAnimating {
     func animatePresentationTransition() {
-        headerIconImageView?.alpha = 1
+        headerIconImageView.alpha = 1
     }
     
     func animateDismissalTransition() {
-        headerIconImageView?.alpha = 0
+        headerIconImageView.alpha = 0
     }
 }
