b9a93e929b119b2df0e7a226689650d37cba8c13
Otto Suess <ottosuess@protonmail.com>
2018-08-22 13:30:27 +0200

    tests: fix broken tests

--- BTCUtilTests/BitcoinURITests.swift
+++ BTCUtilTests/BitcoinURITests.swift
@@ -38,7 +38,7 @@ final class BitcoinURITests: XCTestCase {
     
     func testAddressOrURI() {
         let tests: [(String, String, Satoshi?, String?, String?)] = [
-            ("mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", "mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", nil, nil, nil),
+            ("bitcoin:mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", "mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", nil, nil, nil),
             ("bitcoin:mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p?message=Luke-Jr", "mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", nil, "Luke-Jr", nil),
             ("bitcoin:mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p?amount=20.3&message=Luke-Jr", "mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", 2030000000, "Luke-Jr", nil),
             ("bitcoin:mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p?amount=50&message=Donation%20for%20project%20xyz", "mioi9QugUZFSsSm61Gx4u43s6jYkes2L9p", 5000000000, "Donation for project xyz", nil),
@@ -50,7 +50,7 @@ final class BitcoinURITests: XCTestCase {
         for (uriString, address, amount, memo, lightningFallback) in tests {
             // swiftlint:disable:next force_unwrapping
             let address = BitcoinAddress(string: address)!
-            XCTAssertEqual(BitcoinURI(address: address, amount: amount, memo: memo, lightningFallback: lightningFallback)?.addressOrURI, uriString)
+            XCTAssertEqual(BitcoinURI(address: address, amount: amount, memo: memo, lightningFallback: lightningFallback)?.uriString, uriString)
         }
     }
     
