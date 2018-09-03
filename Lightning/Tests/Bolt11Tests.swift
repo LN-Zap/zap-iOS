e266a4530c1c38a6c1b861d0b999861d0bdd6258
Otto Suess <ottosuess@protonmail.com>
2018-09-03 16:52:22 +0200

    xcode: move tests in their target's folders

--- Lightning/Tests/Bolt11Tests.swift
+++ Lightning/Tests/Bolt11Tests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class Bolt11Tests: XCTestCase {
+    // swiftlint:disable:next function_body_length
     func testBolt11Decoding() {
         let paymentHash = Data(bytes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2])
         
