adab474696b0a71b4ee575c4539b9e9be72e5e7c
Otto Suess <ottosuess@protonmail.com>
2018-12-03 11:43:41 +0100

    refactor: add environment variable to toggle db logging

--- Lightning/Environment.swift
+++ Lightning/Environment.swift
@@ -11,4 +11,8 @@ enum Environment {
     static var useMockApi: Bool {
         return ProcessInfo.processInfo.environment["USE_MOCK_API"] == "1"
     }
+    
+    static var traceDB: Bool {
+        return ProcessInfo.processInfo.environment["TRACE_DB"] == "1"
+    }
 }
