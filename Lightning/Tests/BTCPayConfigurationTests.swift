49d20199b7be36c5dce5c2708cdf4df3b3202543
Otto Suess <ottosuess@protonmail.com>
2019-01-15 12:00:23 +0100

    fix: broken BTCPayConfigurationTests

--- Lightning/Tests/BTCPayConfigurationTests.swift
+++ Lightning/Tests/BTCPayConfigurationTests.swift
@@ -10,45 +10,13 @@ import SwiftLnd
 import XCTest
 
 class BTCPayConfigurationTests: XCTestCase {
-
-    func testDecodeBTCPayConfiguration() {
-        // swiftlint:disable force_unwrapping
-        let data = "{\"configurations\":[{\"type\":\"grpc\",\"cryptoCode\":\"BTC\",\"host\":\"test.test.com\",\"port\":443,\"ssl\":true,\"certificateThumbprint\":null,\"macaroon\":\"02deadbeef0202deadbeef02\"}]}".data(using: .utf8)!
-        
-        let configuration = BTCPayConfiguration(data: data)
-        
-        XCTAssertNotNil(configuration)
-        XCTAssertEqual(configuration?.configurations.count, 1)
-        
-        let item = configuration!.configurations.first!
-        XCTAssertEqual(item.type, "grpc")
-        XCTAssertEqual(item.cryptoCode, "BTC")
-        XCTAssertEqual(item.host, "test.test.com")
-        XCTAssertEqual(item.port, 443)
-        XCTAssertEqual(item.ssl, true)
-        XCTAssertEqual(item.certificateThumbprint, nil)
-        XCTAssertEqual(item.macaroon, "02deadbeef0202deadbeef02")
-    }
-    
-    func testMultipleItems() {
-        let data = "{\"configurations\":[{\"type\":\"grpc\",\"cryptoCode\":\"BTC\",\"host\":\"test.test.com\",\"port\":443,\"ssl\":true,\"certificateThumbprint\":null,\"macaroon\":\"02deadbeef0202deadbeef02\"},{\"type\":\"rest\",\"cryptoCode\":\"BTC\",\"host\":\"test.test.com\",\"port\":443,\"ssl\":true,\"certificateThumbprint\":null,\"macaroon\":\"02deadbeef0202deadbeef02\"}]}".data(using: .utf8)!
-        
-        let configuration = BTCPayConfiguration(data: data)
-        
-        XCTAssertNotNil(configuration)
-        XCTAssertEqual(configuration?.configurations.count, 2)
-        
-        let item = configuration!.configurations.first!
-        XCTAssertEqual(item.type, "grpc")
-    }
-    
     func testGRPCAccessor() {
         let data = "{\"configurations\":[{\"type\":\"rest\",\"cryptoCode\":\"BTC\",\"host\":\"test.test.com\",\"port\":443,\"ssl\":true,\"certificateThumbprint\":null,\"macaroon\":\"02deadbeef0202deadbeef02\"},{\"type\":\"grpc\",\"cryptoCode\":\"BTC\",\"host\":\"test.test.com\",\"port\":443,\"ssl\":true,\"certificateThumbprint\":null,\"macaroon\":\"02deadbeef0202deadbeef02\"},{\"type\":\"rest\",\"cryptoCode\":\"BTC\",\"host\":\"test.test.com\",\"port\":443,\"ssl\":true,\"certificateThumbprint\":null,\"macaroon\":\"02deadbeef0202deadbeef02\"}]}".data(using: .utf8)!
-        let configuration = BTCPayConfiguration(data: data)
+        let configuration = BTCPayRPCConfiguration(data: data)
         
         XCTAssertNotNil(configuration?.rpcConfiguration)
-        XCTAssertNil(configuration?.rpcConfiguration?.certificate)
-        XCTAssertEqual(configuration?.rpcConfiguration?.url.absoluteString, "test.test.com:443")
-        XCTAssertEqual(configuration?.rpcConfiguration?.macaroon, Macaroon(hexadecimalString: "02deadbeef0202deadbeef02"))
+        XCTAssertNil(configuration?.rpcConfiguration.certificate)
+        XCTAssertEqual(configuration?.rpcConfiguration.url.absoluteString, "test.test.com:443")
+        XCTAssertEqual(configuration?.rpcConfiguration.macaroon, Macaroon(hexadecimalString: "02deadbeef0202deadbeef02"))
     }
 }
