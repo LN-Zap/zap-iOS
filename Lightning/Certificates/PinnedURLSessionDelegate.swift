2d5ae62fbbff062683072840e0b75bb2c68141f1
Otto Suess <ottosuess@protonmail.com>
2019-03-07 14:43:41 +0100

    cert: update pinned blockcypher certificate

--- Lightning/Certificates/PinnedURLSessionDelegate.swift
+++ Lightning/Certificates/PinnedURLSessionDelegate.swift
@@ -21,7 +21,7 @@ final class PinnedURLSessionDelegate: NSObject {
     override private init() {
         hosts = [
             PinnedHost(named: "blockchain.info", certificates: ["www.blockchain.com", "DigiCert SHA2 Extended Validation Server CA"]),
-            PinnedHost(named: "blockcypher.com", certificates: ["*.blockcypher.com", "COMODO RSA Domain Validation Secure Server CA"]),
+            PinnedHost(named: "blockcypher.com", certificates: ["*.blockcypher.com", "Sectigo RSA Domain Validation Secure Server CA"]),
             PinnedHost(named: "blockexplorer.com", certificates: ["blockexplorer.com", "CloudFlare Inc ECC CA-2"]),
             PinnedHost(named: "blockstream.info", certificates: ["blockstream.info", "Let's Encrypt Authority X3"])
         ]
