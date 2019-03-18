73ecd1bf2a2adb6b4ea0c5826dfdd14d6b3fb2fa
Knut Nygaard <knutnyg@gmail.com>
2019-03-18 21:45:38 +0100

    Resolve currencySymbol from currencyCode

--- Lightning/Services/ExchangeRates.swift
+++ Lightning/Services/ExchangeRates.swift
@@ -18,6 +18,23 @@ public final class ExchangeRateLoader {
 
     public init() {}
 
+    private static var currencySymbols = buildCurrencySymbols()
+
+    private static func buildCurrencySymbols() -> [String: String] {
+        let identifiers = Locale.availableIdentifiers
+        let formatter = NumberFormatter()
+        var map: [String: String] = [:]
+        for identifier in identifiers {
+            formatter.locale = Locale(identifier: identifier)
+            guard
+                    let code = formatter.currencyCode,
+                    let symbol = formatter.currencySymbol
+                    else { continue }
+            map[code] = symbol
+        }
+        return map
+    }
+
     public func load(completion: @escaping (Result<[FiatCurrency], ExchangeRateLoaderError>) -> Void) {
         guard let url = URL(string: "https://blockchain.info/ticker") else { fatalError("Invalid ticker url.") }
         let task = URLSession.pinned.dataTask(with: url) { data, _, error in
@@ -34,8 +51,8 @@ public final class ExchangeRateLoader {
         task.resume()
     }
 
-    public func loadTicker(ticker: String, symbol: String, completion: @escaping (Result<FiatCurrency, ExchangeRateLoaderError>) -> Void) {
-        guard let url = URL(string: "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC\(ticker)") else { fatalError("Invalid ticker url.") }
+    public func loadTicker(completion: @escaping (Result<[FiatCurrency], ExchangeRateLoaderError>) -> Void) {
+        guard let url = URL(string: "https://apiv2.bitcoinaverage.com/indices/global/ticker/short?crypto=BTC") else { fatalError("Invalid ticker url.") }
 
         let task = URLSession.pinned.dataTask(with: url) { data, _, error in
             if let error = error {
@@ -44,24 +61,25 @@ public final class ExchangeRateLoader {
             } else if let data = data,
                 let jsonData = try? JSONSerialization.jsonObject(with: data, options: []),
                 let json = jsonData as? [String: Any] {
-                if let currencies = ExchangeRateLoader.parseTicker(for: ticker, symbol: symbol, data: json) {
-                    completion(.success(currencies))
-                }
+                let currencies = json
+						.compactMap { ExchangeRateLoader.parseTicker(for: $0, data: $1) }
+				completion(.success(currencies))
             }
         }
         task.resume()
     }
 
-    private static func parseTicker(for ticker: String, symbol: String, data: Any) -> FiatCurrency? {
+    private static func parseTicker(for ticker: String, data: Any) -> FiatCurrency? {
+        let index = ticker.index(ticker.startIndex, offsetBy: 3)
+        let currencyCode: String = ticker.substring(from: index)
         guard
-                let localized = Locale.autoupdatingCurrent.localizedString(forCurrencyCode: ticker),
+                let localized = Locale.autoupdatingCurrent.localizedString(forCurrencyCode: currencyCode),
                 let data = data as? [String: Any],
-                let averages = data["averages"] as? [String: Any],
-                let valueNumber = averages["day"] as? NSNumber
+                let valueNumber = data["last"] as? NSNumber
                 else { return nil }
         let exchangeRate = valueNumber.decimalValue
 
-        return FiatCurrency(currencyCode: ticker, symbol: symbol, localized: localized, exchangeRate: exchangeRate)
+        return FiatCurrency(currencyCode: currencyCode, symbol: ExchangeRateLoader.currencySymbols[currencyCode] ?? currencyCode, localized: localized, exchangeRate: exchangeRate)
     }
 
     private static func parseFiatCurrency(for currencyCode: String, data: Any) -> FiatCurrency? {
