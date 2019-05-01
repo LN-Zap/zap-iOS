5d7ef74d4674e314ec166fc6bbb3caf47ca1129a
Otto Suess <ottosuess@protonmail.com>
2019-05-01 11:05:23 +0200

    dist: add new lnd build script

--- build_lnd.sh
+++ build_lnd.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/Frameworks"
+# Follow the setup instructions to Generate protobuf definitions:
+# https://github.com/lightningnetwork/lnd/blob/master/lnrpc/README.md#generate-protobuf-definitions
+#
+# Install swift-protobuf 1.5.0 (don't use brew): 
+# https://github.com/apple/swift-protobuf
+#
+# Install promobile
+# `go get github.com/halseth/promobile`
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FRAMEWORKDIR=$DIR/Frameworks
+
 cd $GOPATH/src/github.com/lightningnetwork/lnd
-make IOS_BUILD_DIR=$DIR ios
+make rpc
+make IOS_BUILD_DIR=$FRAMEWORKDIR ios
+
+cp lnrpc/rpc.pb.swift $DIR/SwiftLnd/Generated
+cp lnrpc/rpc.grpc.swift $DIR/SwiftLnd/Generated
 
