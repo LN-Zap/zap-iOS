4001607b47b49707e61133d07dab8f4a2cce4261
Otto Suess <ottosuess@protonmail.com>
2019-05-06 16:06:14 +0200

    dist: update build script to use 'go mod vendor'

--- build_lnd.sh
+++ build_lnd.sh
@@ -13,6 +13,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FRAMEWORKDIR=$DIR/Frameworks
 
 cd $GOPATH/src/github.com/lightningnetwork/lnd
+
+make clean
+export GO111MODULE=on
+go mod vendor
+export GO111MODULE=off
 make rpc
 make IOS_BUILD_DIR=$FRAMEWORKDIR ios
 
