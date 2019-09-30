80e1cd298751c533b9bcc7c8a676e4b97b90dabe
Otto Suess <ottosuess@protonmail.com>
2019-09-30 12:05:46 +0200

    dist: remove Lndmobile build script and use Lndmobile github release instead

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -v
+
+# setup submodules
+git submodule update --init --recursive
+
+# download Lndmobile.framework
+VERSION=v0.8.0-beta-rc1
+
+curl -L https://github.com/LN-Zap/Lndmobile.framework/releases/download/$VERSION/Lndmobile.framework.zip --output Lndmobile.framework.zip
+unzip Lndmobile.framework.zip -d Frameworks/
+rm Lndmobile.framework.zip
+
+curl -L https://github.com/LN-Zap/Lndmobile.framework/releases/download/$VERSION/swift-generated.zip --output swift-generated.zip
+unzip -o swift-generated.zip -d SwiftLnd/Generated
+rm swift-generated.zip
+
