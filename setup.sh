2e8afd1474128008fb25f07a005b83d50d9d0892
Christopher Pinski <chitowncrispy@comcast.net>
2019-10-10 21:22:49 -0500

    Updating Lndmobile.framework to v0.8.0-beta-rc2

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ set -v
 git submodule update --init --recursive
 
 # download Lndmobile.framework
-VERSION=v0.8.0-beta-rc1
+VERSION=v0.8.0-beta-rc2
 
 curl -L https://github.com/LN-Zap/Lndmobile.framework/releases/download/$VERSION/Lndmobile.framework.zip --output Lndmobile.framework.zip
 unzip Lndmobile.framework.zip -d Frameworks/
@@ -15,4 +15,3 @@ rm Lndmobile.framework.zip
 curl -L https://github.com/LN-Zap/Lndmobile.framework/releases/download/$VERSION/swift-generated.zip --output swift-generated.zip
 unzip -o swift-generated.zip -d SwiftLnd/Generated
 rm swift-generated.zip
-
