79631a53d7367cb082d7ac4fbe87e6bedcb4c00f
Otto Suess <ottosuess@protonmail.com>
2019-11-13 17:53:02 +0100

    refactor: rename run function to map

--- SwiftLnd/Api/ApiCompletion.swift
+++ SwiftLnd/Api/ApiCompletion.swift
@@ -14,10 +14,10 @@ public typealias ApiCompletion<T> = (Result<T, LndApiError>) -> Void
 /// Used in LightningApi & WalletApi.
 /// Maps an `ApiCompletion<U>` to an `ApiCompletion<T>` with the `map` function
 /// to transform lnd's raw response structs to more swifty version.
-func run<T, U>(_ completion: @escaping ApiCompletion<U>, map: @escaping (T) -> U?) -> ApiCompletion<T> {
+func map<T, U>(_ completion: @escaping ApiCompletion<U>, to: @escaping (T) -> U?) -> ApiCompletion<T> { // swiftlint:disable:this identifier_name
     return { input in
         completion(input.flatMap {
-            if let mapped = map($0) {
+            if let mapped = to($0) {
                 return .success(mapped)
             }
             return .failure(LndApiError.unknownError)
